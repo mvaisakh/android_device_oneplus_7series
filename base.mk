5cca390958900ef6226c3d8d8e95b575c92b892d
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-08-15 13:15:22 +0530

    oneplus7series: Copy permission and feature configs properly
    * Good going CAF
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- base.mk
+++ base.mk
@@ -618,7 +618,7 @@ PRODUCT_PACKAGES += libhealthd.msm
 
 #NANOPB_LIBRARY_NAME := libnanopb-c-2.8.0
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml\
